89925f7e0e0f9a35f96d83e70b0d34042b9d4a20
wiresnchains <dgrachov@icloud.com>
2026-02-12 23:15:18 +0100

    Add enums

---
+++ src/server/enums.ts
@@ -0,0 +1,36 @@
+export enum VehicleNumberPlateType {
+    BLUE_ON_WHITE = 0,
+    BLUE_ON_WHITE_2 = 3,
+    BLUE_ON_WHITE_3 = 4,
+    YELLOW_ON_BLACK = 1,
+    YELLOW_ON_BLUE = 2,
+    NORTH_YANKTON = 5,
+}
+
+export enum VehicleSeat {
+    DRIVER,
+    PASSENGER_1,
+    PASSENGER_2,
+    PASSENGER_3,
+    PASSENGER_4,
+    PASSENGER_5,
+    PASSENGER_6,
+    PASSENGER_7,
+    PASSENGER_8,
+    PASSENGER_9,
+    PASSENGER_10,
+    PASSENGER_11,
+    PASSENGER_12,
+    PASSENGER_13,
+    PASSENGER_14,
+    PASSENGER_15,
+    PASSENGER_16,
+}
+
+export enum VehiclePaint {
+    CLASSIC = 1,
+    METALLIC = 0,
+    MATTE = 3,
+    CHROME = 5,
+    CHAMELEON = 6,
+}
