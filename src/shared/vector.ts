7cedd5cdc789bc95d4fb16e7d57656eb8373ef03
wiresnchains <dgrachov@icloud.com>
2026-02-13 21:47:55 +0100

    Add utility vector methods

--- src/shared/vector.ts
+++ src/shared/vector.ts
@@ -8,4 +8,20 @@ export class Vector3 {
         this.y = y;
         this.z = z;
     }
+
+    public add(vec: Vector3): Vector3 {
+        return new Vector3(this.x + vec.x, this.y + vec.y, this.z + vec.z);
+    }
+
+    public sub(vec: Vector3): Vector3 {
+        return new Vector3(this.x - vec.x, this.y - vec.y, this.z - vec.z);
+    }
+
+    public mul(vec: Vector3): Vector3 {
+        return new Vector3(this.x * vec.x, this.y * vec.y, this.z * vec.z);
+    }
+
+    public div(vec: Vector3): Vector3 {
+        return new Vector3(this.x / vec.x, this.y / vec.y, this.z / vec.z);
+    }
 }
