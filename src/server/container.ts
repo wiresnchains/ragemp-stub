a8c1b771993845d2554ad7aea4d179877d915b8a
wiresnchains <dgrachov@icloud.com>
2026-02-14 18:59:51 +0100

    Update container.ts

--- src/server/container.ts
+++ src/server/container.ts
@@ -5,19 +5,23 @@ import { RagePlayerPool } from './ragemp/player';
 import { RageVehiclePool } from './ragemp/vehicle';
 
 export function createTestContainer(): AtlasContainer {
+    const { get } = createJoaatCache();
+
     return {
         events: {} as any,
         players: {} as any,
         vehicles: {} as any,
-        joaat: createJoaatCache().joaat,
+        joaat: get,
     };
 }
 
 export function createRageContainer(): AtlasContainer {
+    const { get } = createJoaatCache();
+
     return {
         events: new RageEventPool(),
         players: new RagePlayerPool(),
         vehicles: new RageVehiclePool(),
-        joaat: createJoaatCache().joaat,
+        joaat: get,
     };
 }
