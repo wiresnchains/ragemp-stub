7e3687906033ced9d423600bdab9aa0469f68fd4
wiresnchains <dgrachov@icloud.com>
2026-02-15 17:28:26 +0100

    Link all packages together for mocking server state

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
     format: ['esm', 'cjs'],
     dts: { build: true },
     clean: true,
-    external: ['ragemp-atlas/shared'],
+    external: ['ragemp-atlas/shared', 'ragemp-atlas/server', 'ragemp-atlas/client', 'ragemp-atlas/web'],
 });
