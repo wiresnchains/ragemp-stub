f6e14e356482219810b29ac2749568249c50c8ae
wiresnchains <dgrachov@icloud.com>
2026-02-16 23:20:16 +0100

    Export shared vehicle

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,8 +1,10 @@
 export * from './interfaces/event';
 export * from './interfaces/entity';
+export * from './interfaces/vehicle';
 
 export * from './mock/event';
 export * from './mock/entity';
+export * from './mock/vehicle';
 
 export * from './utils/vector';
 export * from './utils/joaat';
