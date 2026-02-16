e860495137f11f364820e8f36fba7664bf19e321
wiresnchains <dgrachov@icloud.com>
2026-02-16 23:42:15 +0100

    Fix shared exports

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,11 +1,15 @@
 export * from './interfaces/event';
 export * from './interfaces/entity';
+export * from './interfaces/ped';
+export * from './interfaces/player';
 export * from './interfaces/vehicle';
+
+export * from './enums/vehicle';
 
 export * from './mock/event';
 export * from './mock/entity';
-export * from './mock/vehicle';
 
 export * from './utils/vector';
 export * from './utils/joaat';
 export * from './utils/type-resolver';
+export * from './utils/color';
