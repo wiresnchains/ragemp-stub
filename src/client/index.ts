4da38f522d5b724a93dd6ad03d947d26b4c61694
wiresnchains <dgrachov@icloud.com>
2026-02-20 15:05:52 +0100

    Fix client exports

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,14 +1,30 @@
 export * from '@/interfaces/container';
 export * from '@/interfaces/entity';
 export * from '@/interfaces/event';
+export * from '@/interfaces/game';
+export * from '@/interfaces/ped';
+export * from '@/interfaces/player';
+export * from '@/interfaces/vehicle';
+export * from '@/interfaces/voice-chat-fx';
 
 export * from '@/enums/ped';
 export * from '@/enums/vector';
+export * from '@/enums/voice-chat-fx';
 
 export * from '@/mock/container';
 export * from '@/mock/entity';
 export * from '@/mock/event';
+export * from '@/mock/game';
+export * from '@/mock/ped';
+export * from '@/mock/player';
+export * from '@/mock/vehicle';
+export * from '@/mock/voice-chat-fx';
 
 export * from '@/ragemp/container';
 export * from '@/ragemp/entity';
 export * from '@/ragemp/event';
+export * from '@/ragemp/game';
+export * from '@/ragemp/ped';
+export * from '@/ragemp/player';
+export * from '@/ragemp/vehicle';
+export * from '@/ragemp/voice-chat-fx';
